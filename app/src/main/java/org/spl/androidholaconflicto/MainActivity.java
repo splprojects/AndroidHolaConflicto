1d400eeb983dbb7ad7f384932b4b9114d54307d0
soniaporter <sonia_porter@hotmail.com>
2017-09-29 19:08:37 +0200

    3 SOY PROGRAMADOR 2 Y PONGO COMENTARIO

--- app/src/main/java/org/spl/androidholaconflicto/MainActivity.java
+++ app/src/main/java/org/spl/androidholaconflicto/MainActivity.java
@@ -15,6 +15,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //COMENTARIO DEL PROGRAMADOR 2
+        //OTRA VEZ
+        
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
